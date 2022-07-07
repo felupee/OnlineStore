fb95d9709f34e805320d99dba2a577540e909c48
felupee <felipesantosof@gmail.com>
2022-07-07 16:28:47 -0300

    alteração de lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
-  
   return (
-    <div className="App">
-     { getCategories() }
-    </div>
+    <div className="App">a</div>
   );
 }
 
