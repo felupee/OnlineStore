05ed2d004f52212d3fd8a943d9b0d32ae5781762
leovcampos <leonardoviniciuscampos@gmail.com>
2022-07-07 16:54:53 -0300

    pequena alteração

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ export async function getCategories() {
   const data = await response.json();
   return data;
 }
-getCategories();
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}}`);
