6bb1cb544a15da6a43872940b188b5ab083371c6
felupee <felipesantosof@gmail.com>
2022-07-07 16:33:15 -0300

    erro de lint

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export async function getCategories() {
 }
 getCategories();
 
-export async function getProductsFromCategoryAndQuery( categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}}`);
   const data = await response.json();
   console.log(data);
