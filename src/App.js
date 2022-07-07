5294c75c2ceace93db538a0d950fa4b4ec2d324a
leovcampos <leonardoviniciuscampos@gmail.com>
2022-07-07 17:20:39 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CarrinhoCompras from './Pages/CarrinhoCompras';
 import InitialPage from './Pages/initialPage';
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ InitialPage } />
+          <Route path="/carrinho" component={ CarrinhoCompras } />
         </Switch>
       </BrowserRouter>
     </div>
